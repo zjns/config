deca8c7374b0b7df74bcdbe5c589598a6ed9b1b3
Kofua <1638183271zjn@gmail.com>
2024-03-22 17:28:01 +0800

    fix: mumu script

--- js/mumu.js
+++ js/mumu.js
@@ -1,9 +1,18 @@
-let body = JSON.parse($response.body);
+if ($response.status === 200) {
+    let body = JSON.parse($response.body);
 
-if (body.code === 0 && body.data.current_device) {
-    let device = body.data.current_device;
-    device.trial_end_at = 1893427200; // 2030-01-01 00:00:00
-    device.trial_status = 1;
+    if (body.code === 0 && body.data) {
+        let data = body.data;
+        data.member_expired_at = 1893427200; // 2030-01-01 00:00:00
+        data.member_status = 1;
+        if (data.current_device) {
+            let device = data.current_device;
+            device.trial_end_at = 1893427200; // 2030-01-01 00:00:00
+            device.trial_status = 1;
+        }
+    }
+
+    $done({ body: JSON.stringify(body) });
+} else {
+    $done({});
 }
-
-$done({ body: JSON.stringify(body) });
